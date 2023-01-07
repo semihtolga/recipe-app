3a10fa3d23dcf81b69230a44ada03f988451613a
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2023-01-07 16:56:36 +0300

    add flex and logincontainer

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import  LoginContainer  from './Login.style'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <LoginContainer>
+      
+    </LoginContainer>
   )
 }
 
---
+++ src/pages/login/Login.style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Flex from "../../components/globalStyles/Flex.style";
+
+
+export const LoginContainer=styled(Flex)`
+  height: 100vh;
+  background-image: url('https://picsum.photos/1600/900');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  
+
+`;
+
+export default LoginContainer;
