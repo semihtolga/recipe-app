d6cb7f963b03fc081fb866109d1e18c5a0dcfd3c
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2023-01-07 16:05:01 +0300

    global style added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {GlobalStyles} from '../components/globalStyles/GlobalStyles'
+import Navbar from "./../components/nav/Navbar"
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
+    <BrowserRouter>
+    <GlobalStyles />
+    <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default AppRouter
+export default AppRouter;
