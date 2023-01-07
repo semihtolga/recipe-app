b7b05a55721ce0c8561d686675c5bdb7120ec1f9
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2023-01-07 16:26:21 +0300

    add private

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {GlobalStyles} from '../components/globalStyles/GlobalStyles'
 import Navbar from "./../components/nav/Navbar"
+import Home from "../pages/home/Home"
+import Login from "../pages/login/Login"
+import About from "../pages/about/About"
+import Detail from "../pages/detail/Detail"
 
 const AppRouter = () => {
   return (
@@ -9,7 +13,14 @@ const AppRouter = () => {
     <GlobalStyles />
     <Navbar />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home/>} />
+        <Route path="login" element={<Login/>} />
+        <Route path="about" element={<About/>} >
+          <Route path="" element={<About/>}/>
+          </Route>
+        <Route path="detail" element={<Detail/>} >
+          <Route path="" element={<Detail/>} />
+          </Route>
       </Routes>
     </BrowserRouter>
   )
