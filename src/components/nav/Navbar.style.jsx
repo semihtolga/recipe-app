09e6da000dace16667809f0fa25113326b8093f2
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2023-01-09 22:43:40 +0300

    add styling

--- src/components/nav/Navbar.style.jsx
+++ src/components/nav/Navbar.style.jsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
-import Flex  from "../globalStyles/Flex.style";
+import Flex from "../globalStyles/Flex.style";
 import { Link } from "react-router-dom";
 
 export const Nav = styled(Flex)`
-padding: 1rem 1.5rem;
+  padding: 1rem 1.5rem;
 `;
 
-export const Brand=styled(Link)`
-color:${({theme})=>theme.colors.logoColor}
-
+export const Brand = styled(Link)`
+  color: ${({ theme }) => theme.colors.logoColor};
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 2rem;
+  span {
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.mainColorColor};
+  }
 `;
