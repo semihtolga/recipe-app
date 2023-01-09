901ee17e552d632b4ed743c7ce2a9ab1da790fda
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2023-01-09 21:39:31 +0300

    add session storage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import  LoginContainer  from './Login.style'
+import  {FormContainer, Header, LoginContainer, StyledButton, StyledForm, StyledImg, StyledInput}  from './Login.style'
+import meal from "../../assets/meal.svg"
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const userInfo= {
+    username:"admin"
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/-1");
+    sessionStorage.setItem("user",JSON.stringify(userInfo))
+  }
+
   return (
     <LoginContainer>
-      
-    </LoginContainer>
-  )
-}
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>
+          {"<CW/>"}Recipe
+        </Header>
+        <StyledForm onSubmit={handleSubmit} >
+        <StyledInput placeholder="ENTER USERNAME" type="text" />
+        <StyledInput placeholder="ENTER PASSWORD" type="password"/>
+        <StyledButton type='submit' >LOGIN</StyledButton>
+        </StyledForm>
 
-export default Login
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
